9db3556756c6e94b46de126856b37c688594d887
Oliver Schafeld <oliver@schafeld.com>
2020-12-18 10:52:28 +0100

    add hot reload trick

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -4,12 +4,28 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const rootElement = document.getElementById('root');
+
+function render() {
+  ReactDOM.render(<App />, rootElement)
+}
+
+// Trick to trigger hot reload without page reload
+if (module.hot) {
+  module.hot.accept('./App', function() {
+    setTimeout(render);
+  })
+}
+
+render();
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//   {/* Remove 'strict mode' wrapper if annoyed by excessive warnings in console. */}
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
