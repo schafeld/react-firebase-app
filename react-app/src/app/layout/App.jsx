c1a53f5188bc6a669a6dca3b8491029d4dc97602
Oliver Schafeld <oliver@schafeld.com>
2020-12-20 12:06:33 +0100

    add Semantic UI

--- react-app/src/app/layout/App.jsx
+++ react-app/src/app/layout/App.jsx
@@ -1,7 +1,14 @@
+import { Button } from "semantic-ui-react";
+
 function App() {
   return (
     <div className="App">
-      <h1>Olli's App</h1>  
+      <h1>Olli's App</h1>
+      <button type="button" className="ui icon red button">
+        <i className="user icon"></i>CSS Button
+      </button>
+
+      <Button icon="user" content="React Semantic UI Button" color="green" loading="true" />
     </div>
   );
 }
