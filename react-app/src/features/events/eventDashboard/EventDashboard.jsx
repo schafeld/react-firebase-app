87805ed06b515342f25403f4aa50f3c1894a7d5e
Oliver Schafeld <oliver@schafeld.com>
2020-12-23 14:41:05 +0100

    pass on mock data as property

--- react-app/src/features/events/eventDashboard/EventDashboard.jsx
+++ react-app/src/features/events/eventDashboard/EventDashboard.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import { Grid } from 'semantic-ui-react';
 import EventList from './EventList';
 import EventForm from '../eventForm/EventForm';
+import { sampleData } from '../../../app/api/sampleData';
 
 export default function EventDashboard() {
   return(
     <Grid>
       <Grid.Column width={10} >
-        <EventList />
+        <EventList events={sampleData} />
       </Grid.Column>
       <Grid.Column width={6} >
         <EventForm />
