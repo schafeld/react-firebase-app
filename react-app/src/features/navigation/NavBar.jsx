957a6a1b5fe1c503ece08e4e1cf5bc4e61a254b8
Oliver Schafeld <oliver@schafeld.com>
2021-01-02 14:27:45 +0100

    add route links

--- react-app/src/features/navigation/NavBar.jsx
+++ react-app/src/features/navigation/NavBar.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import { Button, Container, Menu, MenuItem } from 'semantic-ui-react';
 
 export default function NavBar({setFormOpen}) {
   return (
     <Menu inverted fixed='top'>
     <Container>
-      <Menu.Item header>
+      <Menu.Item as={NavLink} exact to='/' header>
         <img src='/assets/logo.svg' alt='logo' id="logo" />
         Olli's Event App
       </Menu.Item>
-      <MenuItem name='Events' />
-      <MenuItem>
-        <Button onClick={() => setFormOpen(true)} positive inverted content='Create Event' />
+      <MenuItem name='Events' as={NavLink} to='/events' />
+      <MenuItem as={NavLink} to='/createEvent'>
+        <Button positive inverted content='Create Event' />
       </MenuItem>
       <MenuItem position='right'>
         <Button basic inverted content='Login' />
