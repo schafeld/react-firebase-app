322eff05429aa2cba428a14d916d812dc14e302f
Oliver Schafeld <oliver@schafeld.com>
2021-01-05 18:27:44 +0100

    add component boilerplate for event details page

--- react-app/src/features/events/eventDashboard/EventListItem.jsx
+++ react-app/src/features/events/eventDashboard/EventListItem.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Button, Icon, Item, ItemGroup, List, Segment } from 'semantic-ui-react';
 import EventListAttendee from './EventListAttendee';
 
@@ -41,7 +42,7 @@ export default function EventListItem({event, selectEvent, deleteEvent}) {
           content="Delete"
         />
         <Button
-          onClick={() => selectEvent(event)}
+          as={Link} to={`/events/${event.id}`}
           color="teal"
           floated="right"
           content="View"
