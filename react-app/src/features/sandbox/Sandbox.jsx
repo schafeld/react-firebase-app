20740edb84f115ff4f246fcabaa6122711ca0375
Oliver Schafeld <oliver@schafeld.com>
2021-01-06 18:07:09 +0100

    add demo redux actions and buttons

--- react-app/src/features/sandbox/Sandbox.jsx
+++ react-app/src/features/sandbox/Sandbox.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { Button } from 'semantic-ui-react';
+import { INCREMENT_COUNTER, DECREMENT_COUNTER } from './testReducer';
 
 export default function Sandbox() {
-  const data = useSelector(state => state.data)
+  const dispatch = useDispatch();
+  const data = useSelector(state => state.data);
+
   return (
     <>
       <h2>Testing 123</h2>
       <h3>The data is: {data}</h3>
+      <Button onClick={ () => dispatch({type: INCREMENT_COUNTER}) } content='Increment' color='green' />
+      <Button onClick={ () => dispatch({type: DECREMENT_COUNTER}) } content='Decrement' color='red' />
     </>
   )
 }
--- react-app/src/features/sandbox/testReducer.js
+++ react-app/src/features/sandbox/testReducer.js
@@ -1,7 +1,23 @@
+export const INCREMENT_COUNTER = 'INCREMENT_COUNTER';
+export const DECREMENT_COUNTER = 'DECREMENT_COUNTER';
+
 const initialState = {
   data: 42
 }
 
-export default function testReducer(state = initialState) {
-  return state
+export default function testReducer(state = initialState, action) {
+  switch (action.type) {
+    case INCREMENT_COUNTER:
+      return {
+        ...state,
+        data: state.data + 1
+      }
+      case DECREMENT_COUNTER:
+        return {
+          ...state,
+          data: state.data - 1
+        }
+      default:
+        return state
+  }
 }
