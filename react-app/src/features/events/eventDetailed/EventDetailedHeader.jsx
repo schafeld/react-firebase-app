ffbf2b15d410d3ef20535efeca063a80c95da7bb
Oliver Schafeld <oliver@schafeld.com>
2021-01-08 16:35:13 +0100

    pass event detail data from Redux to components

--- react-app/src/features/events/eventDetailed/EventDetailedHeader.jsx
+++ react-app/src/features/events/eventDetailed/EventDetailedHeader.jsx
@@ -15,12 +15,12 @@ const eventImageTextStyle = {
   color: 'white'
 };
 
-export default function EventDetailedHeader() {
+export default function EventDetailedHeader({event}) {
   return (
     <Segment.Group>
     <Segment basic attached="top" style={{padding: '0'}}>
         {/* TODO: Use proper image. */}
-        <Image src={`/assets/categoryImages/drinks.jpg`} fluid style={eventImageStyle} />
+        <Image src={`/assets/categoryImages/${event.category}.jpg`} fluid style={eventImageStyle} />
 
         <Segment basic style={eventImageTextStyle} >
             <Item.Group>
@@ -28,12 +28,12 @@ export default function EventDetailedHeader() {
                     <Item.Content>
                         <Header
                             size="huge"
-                            content='Event Title'
+                            content={event.title}
                             style={{color: 'white'}}
                         />
-                        <p>Event Date</p>
+                        <p>{event.date}</p>
                         <p>
-                            Hosted by <strong>Bob</strong>
+                            Hosted by <strong>{event.hostedBy}</strong>
                         </p>
                     </Item.Content>
                 </Item>
