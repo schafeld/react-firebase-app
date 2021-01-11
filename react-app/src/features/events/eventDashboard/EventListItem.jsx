3458fb2acf1faf9e9d12c536e6d96725881b6d79
Oliver Schafeld <oliver@schafeld.com>
2021-01-11 11:20:31 +0100

    use Redux action to delete event

--- react-app/src/features/events/eventDashboard/EventListItem.jsx
+++ react-app/src/features/events/eventDashboard/EventListItem.jsx
@@ -2,8 +2,13 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Button, Icon, Item, ItemGroup, List, Segment } from 'semantic-ui-react';
 import EventListAttendee from './EventListAttendee';
+import { useDispatch } from 'react-redux';
+import { deleteEvent } from '../eventActions';
 
-export default function EventListItem({event, selectEvent, deleteEvent}) {
+export default function EventListItem({event}) {
+
+  const dispatch = useDispatch();
+
   return (
     <Segment.Group>
       <Segment>
@@ -36,7 +41,7 @@ export default function EventListItem({event, selectEvent, deleteEvent}) {
       <Segment clearing>
         <div className="event-description">{event.description}</div>
         <Button
-          onClick={() => deleteEvent(event.id)}
+          onClick={() => dispatch(deleteEvent(event.id))}
           color="red"
           floated="right"
           content="Delete"
