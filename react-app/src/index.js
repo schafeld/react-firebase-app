2841097c8a0c70e7ed0a97d79141eb36133f6991
Oliver Schafeld <oliver@schafeld.com>
2021-01-12 11:52:31 +0100

    scroll to top on route change, through hook.

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -9,6 +9,7 @@ import './app/layout/styles.css';
 import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
 import { configureStore } from './app/store/configureStore';
+import ScrollToTop from './app/layout/ScrollToTop';
 
 const store = configureStore()
 const rootElement = document.getElementById('root');
@@ -17,6 +18,7 @@ function render() {
   ReactDOM.render(
     <Provider store={store}>
       <BrowserRouter>
+      <ScrollToTop />
         <App />
       </BrowserRouter>
     </Provider>
